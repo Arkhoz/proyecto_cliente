09c32631432316b577ae155589f6327d0d99456d
Victor Marcos <victor.marcosmoldes@gmail.com>
2024-07-16 13:09:18 +0200

    Inicio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,4 +37,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.3")
+
 }
--- app/src/main/java/com/example/autoescuela/funciones/Registro.java
+++ app/src/main/java/com/example/autoescuela/funciones/Registro.java
@@ -1,6 +1,9 @@
 package com.example.autoescuela.funciones;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.autoescuela.R;
+import com.google.gson.Gson;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
 
 public class Registro extends AppCompatActivity {
 
@@ -22,5 +30,34 @@ public class Registro extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        EditText usuario=findViewById(R.id.tUsuarioReg);
+        EditText contrasenia=findViewById(R.id.tPassReg);
+        EditText nombre=findViewById(R.id.tNombreReg);
+        EditText correo=findViewById(R.id.tCorreoReg);
+        EditText telefono=findViewById(R.id.tTelefonoReg);
+        EditText dni=findViewById(R.id.tDniReg);
+
+        ImageButton ibregistrarse=findViewById(R.id.ibRegistro);
+
+        ibregistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OkHttpClient cliente = new OkHttpClient();
+                Gson gson = new Gson();
+
+                RequestBody body = new FormBody.Builder().add(usuario.getText()+"",
+                        contrasenia.getText()+"",
+                        nombre.getText()+"",
+                        correo.getText()+"",
+                        telefono.getText()+"",
+                        dni.getText()+"").build();
+
+
+
+
+            }
+        });
+
+
     }
 }
